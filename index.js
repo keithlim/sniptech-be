bb8ed9c5995468c3dd535cd0c6bba93931d0ed11
keithlim <keithlck96@gmail.com>
2021-09-06 18:40:41 +0800

    Added Cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = require('./src/app');
+const cors = require('cors');
+
+app.use(cors());
 
 // create HTTP server
 const server = require('http').createServer(app);
